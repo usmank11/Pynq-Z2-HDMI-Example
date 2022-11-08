e0b96dca69f879f46dcd169977a8a84ee5b9fc1e
Usman <usmank6009@gmail.com>
2022-11-07 23:16:55 -0500

    HDMI example

---
+++ hdmi.ip_user_files/sim_scripts/design_1/xcelium/run.f
@@ -0,0 +1,109 @@
+-makelib xcelium_lib/xilinx_vip -sv \
+  "C:/Xilinx/Vivado/2018.1/data/xilinx_vip/hdl/axi4stream_vip_axi4streampc.sv" \
+  "C:/Xilinx/Vivado/2018.1/data/xilinx_vip/hdl/axi_vip_axi4pc.sv" \
+  "C:/Xilinx/Vivado/2018.1/data/xilinx_vip/hdl/xil_common_vip_pkg.sv" \
+  "C:/Xilinx/Vivado/2018.1/data/xilinx_vip/hdl/axi4stream_vip_pkg.sv" \
+  "C:/Xilinx/Vivado/2018.1/data/xilinx_vip/hdl/axi_vip_pkg.sv" \
+  "C:/Xilinx/Vivado/2018.1/data/xilinx_vip/hdl/axi4stream_vip_if.sv" \
+  "C:/Xilinx/Vivado/2018.1/data/xilinx_vip/hdl/axi_vip_if.sv" \
+  "C:/Xilinx/Vivado/2018.1/data/xilinx_vip/hdl/clk_vip_if.sv" \
+  "C:/Xilinx/Vivado/2018.1/data/xilinx_vip/hdl/rst_vip_if.sv" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib -sv \
+  "C:/Xilinx/Vivado/2018.1/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "C:/Xilinx/Vivado/2018.1/data/ip/xpm/xpm_fifo/hdl/xpm_fifo.sv" \
+  "C:/Xilinx/Vivado/2018.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "C:/Xilinx/Vivado/2018.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/axi_infrastructure_v1_1_0 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/ec67/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/smartconnect_v1_0 -sv \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/02c8/hdl/sc_util_v1_0_vl_rfs.sv" \
+-endlib
+-makelib xcelium_lib/axi_protocol_checker_v2_0_2 -sv \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/3755/hdl/axi_protocol_checker_v2_0_vl_rfs.sv" \
+-endlib
+-makelib xcelium_lib/axi_vip_v1_1_2 -sv \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/725c/hdl/axi_vip_v1_1_vl_rfs.sv" \
+-endlib
+-makelib xcelium_lib/processing_system7_vip_v1_0_4 -sv \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/b193/hdl/processing_system7_vip_v1_0_vl_rfs.sv" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_processing_system7_0_0/sim/design_1_processing_system7_0_0.v" \
+-endlib
+-makelib xcelium_lib/v_tpg_v7_0_10 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ip/design_1_v_tpg_0_0/hdl/v_tpg_v7_0_rfs.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_v_tpg_0_0/sim/design_1_v_tpg_0_0.v" \
+-endlib
+-makelib xcelium_lib/lib_cdc_v1_0_2 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/ef1e/hdl/lib_cdc_v1_0_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/proc_sys_reset_v5_0_12 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/f86a/hdl/proc_sys_reset_v5_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_rst_ps7_0_100M_0/sim/design_1_rst_ps7_0_100M_0.vhd" \
+-endlib
+-makelib xcelium_lib/axi_lite_ipif_v3_0_4 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/cced/hdl/axi_lite_ipif_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/v_tc_v6_1_12 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/6694/hdl/v_tc_v6_1_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_v_tc_0_0/sim/design_1_v_tc_0_0.vhd" \
+-endlib
+-makelib xcelium_lib/v_vid_in_axi4s_v4_0_8 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/d987/hdl/v_vid_in_axi4s_v4_0_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/v_axi4s_vid_out_v4_0_9 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/9a07/hdl/v_axi4s_vid_out_v4_0_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_v_axi4s_vid_out_0_0/sim/design_1_v_axi4s_vid_out_0_0.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/d57c/src/ClockGen.vhd" \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/d57c/src/SyncAsync.vhd" \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/d57c/src/SyncAsyncReset.vhd" \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/d57c/src/DVI_Constants.vhd" \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/d57c/src/OutputSERDES.vhd" \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/d57c/src/TMDS_Encoder.vhd" \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/d57c/src/rgb2dvi.vhd" \
+  "../../../bd/design_1/ip/design_1_rgb2dvi_0_0/sim/design_1_rgb2dvi_0_0.vhd" \
+-endlib
+-makelib xcelium_lib/generic_baseblocks_v2_1_0 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/b752/hdl/generic_baseblocks_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/fifo_generator_v13_2_2 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/7aff/simulation/fifo_generator_vlog_beh.v" \
+-endlib
+-makelib xcelium_lib/fifo_generator_v13_2_2 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/7aff/hdl/fifo_generator_v13_2_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/fifo_generator_v13_2_2 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/7aff/hdl/fifo_generator_v13_2_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_data_fifo_v2_1_15 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/d114/hdl/axi_data_fifo_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_register_slice_v2_1_16 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/0cde/hdl/axi_register_slice_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_protocol_converter_v2_1_16 \
+  "../../../../hdmi.srcs/sources_1/bd/design_1/ipshared/1229/hdl/axi_protocol_converter_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_auto_pc_0/sim/design_1_auto_pc_0.v" \
+  "../../../bd/design_1/sim/design_1.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
---
+++ hdmi.srcs/sources_1/bd/design_1/ip/design_1_v_tpg_0_0/hdl/verilog/design_1_v_tpg_0_0_tpgPatternMask.v
@@ -0,0 +1,78 @@
+`pragma protect begin_protected
+`pragma protect version = 1
+`pragma protect encrypt_agent = "XILINX"
+`pragma protect encrypt_agent_info = "Xilinx Encryption Tool 2014"
+`pragma protect key_keyowner = "Aldec", key_keyname= "ALDEC15_001", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 256)
+`pragma protect key_block
+D5QoicW6bpgEQlqEr8OGAiqGZ/nmGzx9E/dy1BnxVqM1eGGZQ4J/BaK69JpGLEl5MZzjWAm846Qw
+lBbM0yaBQ95fPruL/e9SZDcfi8jlSiSY7Cnh0tJTm4teoVbD6jlm3GHlBJRUCr+zrxSjq8EklpU2
+9+jovhTT4djJ8wr0k/PO45OdYmZuSEbQzBfR2cxsT/xzyRgcPJwQMuy05ltJjW0HOQRWRZ5DkEQa
+6CrYbn4nS3z9zkI7cXJvw3rjmNd1W6z9bxRAK4MbAEAd2kmSVjxCo/u3SumuZ9az7y/ZITOZnNEg
+5HZeWxlWa7Ux8DY/0XUQSvBwqAeKIMc1vQfsAg==
+`pragma protect key_keyowner = "Cadence Design Systems.", key_keyname= "cds_rsa_key", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 64)
+`pragma protect key_block
+FI04D70RxvcSHm/xv9RKz1zEX2JD+IVMRn8V3tfhuUv2nvR9A2NOlO/tVlegjN1Z7WrSroTEehzf
+h01VB00UAA==
+`pragma protect key_keyowner = "Mentor Graphics Corporation", key_keyname= "MGC-VERIF-SIM-RSA-2", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 256)
+`pragma protect key_block
+RfeChAtQpX87SVul/DLwxuHTJCS1Rz7KaohTZt69ZoWPzxcX0Lp3kBc9qBTOWb1xLZl75o9Er99e
+XISnUJE2NsN1h0hwvJObYCexCvBcOuUejM7gtAdinG9jA3/LwYM2XOYrZelPiLiqmGaX0+Ko8XGx
+GjOFvNAeaHUGxZv5P9eb2QoQj3WzUfml+W1UZS56otFU86RpnWCngqhPO2058fofI0BjIuA2darX
+8wcJO+5s8bQRAflnz5Wq0kJabx9WJErUNRQWLp2YiQEb8AYm7miLqHjTZnNLt9N32M+7ftwfLAVU
+fJtAnSovv3SDf87VwywqrgsR6SYipZsuTCgAtw==
+`pragma protect key_keyowner = "Synopsys", key_keyname= "SNPS-VCS-RSA-2", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 128)
+`pragma protect key_block
+HF7PfKnxbBPQknkWAEFV+h1h238WV30zp5mrSvo4ei/ZPUPoaOyTG/NmAR6dtWHqzuXNrl1GOFt3
+ydlSokgSD3YXWQ0gU0+mHQ5jsgLmiF92na4RByYPRtZ9nGlor0zD/+/JUQtu0JPCJ6N8ChvorbXf
+ufFvaxptrk47Ur/CeiY=
+`pragma protect key_keyowner = "Xilinx", key_keyname= "xilinxt_2017_05", key_method = "rsa"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 256)
+`pragma protect key_block
+oinzP6rn1uset+th64At/7wtmy20c0cAW/3ZdSP1iojU+1MOBL/A/swzKtCWW4IWf+GfgpHlwNRd
+Fkgu1tn/Bzs5ZS7dJuJzcJlJvySj74IJjWO5r8AQkT5TGBULXpvu6puofdM9lAF77HFLvTwGqDlw
+UZsh9eZViTS7NGfcx2qMfZK15UEG7DrltlgHvMhEY7M2FIgPGH/SmAzNzquPG1h+C/toWLcs9eZh
+huAJSLN54g5xqmtAzGlLoZl0xcf6aLfrvth1XpwwocGoo0P1ZxN4YB5AqxifjZq1DnTo2w1vcN+8
+Gcy8TpeRt2aKK9PeksV3Tk0RJ6xWR+TVAeh9zw==
+`pragma protect data_method = "AES128-CBC"
+`pragma protect encoding = (enctype = "BASE64", line_length = 76, bytes = 1968)
+`pragma protect data_block
+jd//f+4KDkEiznLXCK8Savjg2Y57FqYI+9jEJk4Sn6RMID90dD/v6Fi09qJYBhBcd2AQI0ZVwFRz
+e3SqnPMartUq1CdvdcJBhRtsAWamrd5L1LR8VjtDE0bPl7vmFGEltgrKxugxYp2ArX3ZVhXp9nmT
+KhvJM4NhIwrbebRUMTWaNSnxnnl+8+VRrWhCGxnNyRRdSmJk/wB404ytd0Th7gX2mvLGYwXHI1Zn
+cw2SBeAuXxW9u/E4mzA1JErDjTqc7EyNS4FQoaMFU7zPqlT9WWG0hlZGukh0nVg/icNngwb/Votw
+RRs8HMgiq2lTkadFsHqazggX9Rm7aa35RFmab3CqSTPxK3NRwLHj/KVsx4TgsaMg4Q6RF0yRP0Ji
+YcpcfwScN0GTy9+39OihqkpVVudhtCM50916rJJL+7knT7j/25h9VO9Y/d2+PbCBi1FZOLxX8Fy+
+5fpm8mJWA6q6YA0lnGYjMPy5APPZiDqzkoD+o3P8BKog8XVLo2um1I4XlrmkcxIEqQzIXhH3ZpKz
+DVrFQjGAq3ZVdZjLKJCyu2TO+xmqmj8tzHi43pYtQz1PCpB1hrU5NOQd4BA/crARusALiDq7PSaS
+CyKSnsZ8sM9/c8HCHyrHQ0qoLItFs72M6S4gayzGXmB2Or07ii30sMJnRhqG1r4/jAxnwiS9RhyO
++/W3ZCKTaLJEkxLWFRAE3sked2jVqDB60osTRH6Lg55+xXcH41OY9xwMb3hmMEWbrdTxiehBVfNY
+s17BdDHvZFcjxTqF2qk5/SX2yf/VD//TLBySRwGz7MXKbDWpEECzZcN9Y3v774hMGQ4//PGCKHTU
+nK9yTsiXvmDm9hU+Ji+2QH9Bg5Jir1oR8Rskrn0ijYaIVgI/FtV906Z+tn+dVYYWfkzMAd7QMt/C
+E3ERQIMf0cF0iIe5PEVwXknhE2wxbOq3My+hclR281bK2CEr3sJRH4Ur0Fft5QbvCx8TPjoCZHIS
+xMu6JEqGkK4hSTkL60PJzqOIhq9ATyC3ypQS77R2k86FDIWplRFKtVPt4fdDjSUAqxsttXlJ7YgF
+2nDjaoQsxM+a/3EfVvO1sCbadcYs6gy8PuSlIbzP5tbd0Fn/keWF6D/x8x2lbdZobarsM93dt4+E
+j/6Yt9cX/OrqhHchalLbGcIRXP7uhKLVFtIZCsOZ5Y/xjd/lOS/lDxSRCUDyuvLJhf0i18V9EzbR
+J3v7JCnOnOrLfSzzMFndwbRLaWAgRS2LjghtiPkCC62W4hvuQg6TV/HfSSa+2EDq0+im5gXtIicC
+NA+7lp8rEHrPAYt9XgYbCSDGL/FgoifgK5PFSU3WVV3pJ7hoIAytAY4yEweMftcXkSAo1fRweBUP
+gZKikkzwlFkAiv4oOc5eB65Efq1z2GZ9W785WoNL+iefSSUx+nt4erFzMeRNyns3jdjocO6VeQcg
+NwO2rEUvxNadlnkx8WzRCvyUukIV+sPykchfU6/TLb2+Dl6valJnk99ismODOv/Axi2U3YYf8qrA
+m2fd5C+QF24K4pwolGkjZD1xr/+K7+WApxbbBeNjlLoZ+ENeS4XEGq0kdQhp6qC6Mt6CxHtJj6J+
+s7bqLawi8ADEcpfDy4R1cG2UQgnUP6njzoPNlyCmEFFwR+iOA/DmTQongBxJl07KHLHK/08/UycA
+p6GOHRU/JDubP2jWfe//44L2tf+FG0OVb9z/KdlmrMfewsmZnGRJiYW1mjApNAB40SdmVZaviUWE
+I7D7WurvB4qEYQhPf5C9HD0Fg1vKjsw/pUNBxGVs8xh1IPk+fW5YerlC2ksXcUQOG3zDPhYt/JoQ
+jAMVpi+Ofvnz0HZo2bRDRGJ+jC5sfcYqPCGx3JYNjke6aB85q8ykY8BStPRMpgmJTeHVE9pdQucy
+Fco8h4AOFKZ+SZgrNKsIcskNazyqwznOEwmRF1QLPgLVknGOXLi+RUj4LJR7+GJMait+KveMLi1D
+rLX0693hrlmyZwcaiVUj8nTiXOnXV38NGgdhMamAdjJ0CvQAJ862/bc+GRkSuYujR15H5kXxEUT0
+gxx38ncaLuStFYeE4BofZBufQidcOSzv/0FtUKAZyTy/jfA2njw1fVMJi7t9D6MxKPSmedZRtKur
+tpCLGFyrZgXhcx64y/zNWqAQX/ljLlRfze/oVAYqaxV80aUZUR2BZT+jxdLdvB7/9Yw785RDKmX6
+EQE3a+ZCo2t6yZf9PCVmSeUGvqUSeLLkn51ozrwZPE0KlcJGFxepuC+l5tMco65UqIAtgmf/0Ipm
+VVXYptOM6yIaBuyFzlm+Q9WD+OR97SMdFUn8k6D5tV86ZhZjBj5oH+GnMbqqvigBnk23XOcDGIB4
+4yEHxLxl6hrVXx4lEwMvwjUb39AnNkn2x68cMFcBIGq6kU40f28PP4LJa78+cGy7CYzpX0Z2Wf1N
+/9NW0o4tcLbmYCQ1SHnjvQLMdP5+5WqzB06KDyJaBUPs7ZIKct+E2r5qoRylHYk3E9A31Tb6Y8OR
+gxMzXFvBLOpMgvW292npRltjINJKe7ANDYlBcw6RxAPUlQNzDSPAG2AAcz8Z08kCBcMPKKoga01G
+NcJXUn2jK5kVqqydqimw4XcZuaENnNKg+YRRj3B1
+`pragma protect end_protected
